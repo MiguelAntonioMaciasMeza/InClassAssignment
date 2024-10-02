8c026e79bf7d6414f990a01c69c6920f34ef8c70
Jacob Berry <jacobberry777@yahoo.com>
2024-10-01 20:26:38 -0700

    db connect and schema

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import the Express module
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,11 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+const mongoConnect = async () => {
+  await mongoose.connect("mongodb://localhost:27017")
+  console.log("DB connected")
+}
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
