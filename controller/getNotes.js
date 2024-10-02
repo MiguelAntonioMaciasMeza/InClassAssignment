9016ea5d231c58a099b5d75f555ecef3f48c4f4d
Ndanis2901 <ndanis@csu.fullerton.edu>
2024-10-01 21:48:31 -0700

    improve getNote function

--- controller/getNotes.js
+++ controller/getNotes.js
@@ -4,10 +4,14 @@ import {getUserById} from "../services/notes.services.js"
 export const getUserNotes = async (req,res) =>{
   try{
     const id = req.param.id;
+    if (!id) {
+      return res.status(400).json({ message: "ID parameter is required" });
+      }
     const note = getUserById(id);
     console.log(note);
     res.status(200).json({note});
   }catch(err){
     console.log(err)
   }
+  res.status(500).json({ message: "Error retrieving the note", error: err.message });
 }
